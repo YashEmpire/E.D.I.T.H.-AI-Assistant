f3a29727a6c918c961e739fc6c299a791f7dc042
Yash <110057015+YashEmpire@users.noreply.github.com>
2023-07-29 02:47:56 +0530

    Delete Config.py

--- Config.py
+++
@@ -1,4 +0,0 @@
-
-apikey = "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"
-#apikey = "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"
-#apikey = "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"
